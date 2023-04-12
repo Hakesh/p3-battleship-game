b5e1496d20713b6a24bc0d225aa7be4d19776d2d
Hakesh <>
2023-04-12 17:32:09 +0000

    Add content to main() function for the main menu

--- run.py
+++ run.py
@@ -1,10 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def main():
-    print("hello world")
+    """
+    The starting menu for the game, lets the user read the rules for Hangman
+    or starts the game depending on user input
+    """
+
+    print("Please select what you want to do next: ")
+    options_text = """
+    1. Start Game
+    2. Read the rules for Hangman
+    """
+    print(options_text)
+
+    options_input = input("Enter what you want to do next here: ")
+    if options_input == 1:
+        print("placeholder")
+    
+    elif options_input == 2:
+        print("placeholder")
+
+    else:
+        print("Invalid option! Please only select between option 1 and 2.")
 
 
-print("Hello and welcome to a gamg of hangman!")
+print("-" * 39)
+print("Hello and welcome to a game of Hangman! \n")
 main()
