7252be3182f48e6f106d48330eb983cd4701b143
Hakesh <>
2023-04-14 18:02:10 +0000

    Add rules function for the rules page of the main menu. Also made words_list.py file for random word selecting function later

--- run.py
+++ run.py
@@ -1,8 +1,50 @@
+from words_list import words
+import os
+
+
+
+
+def rules():
+    clear_terminal()
+    print("-" * 30)
+    rules_text = """The rules are as follows:\n
+    1. 
+    2.
+    3.
+    4."""
+    print(rules_text)
+    
+    option_choice = False
+    while option_choice is False:
+        try:
+            option_choice = input("\nAre you ready to play? Y/N: ")
+
+            if option_choice == "y" or option_choice == "Y":
+                option_choice = True
+                print("placeholder")
+
+            elif option_choice == "n" or option_choice == "N":
+                option_choice = True
+                clear_terminal()
+                main()
+
+            else:
+                raise ValueError("only Y or N are valid options")
+        except ValueError as e:
+            print(f"\nInvalid Data: {e}.\n")
+            option_choice = False
+
+def clear_terminal():
+    """
+    This function clears the terminal when called upon. 
+    Taken from here: https://stackoverflow.com/questions/2084508/clear-terminal-in-python
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main():
     """
-    The starting menu for the game, lets the user read the rules for Hangman
+    The main menu for the game, lets the user read the rules for Hangman
     or starts the game depending on user input
     """
 
@@ -13,17 +55,17 @@ def main():
     """
     print(options_text)
 
-    wrong_option = True
-    while wrong_option:
+    option_choice = False
+    while option_choice is False:
         options_input = input("Enter what you want to do next here: ")
 
         if options_input == "1":
             print("placeholder")
-            wrong_option = False
-    
+            option_choice = True
+
         elif options_input == "2":
-            print("placeholder")
-            wrong_option = False
+            option_choice = True
+            rules()
 
         else:
             print("\n---------")
---
+++ words_list.py
@@ -0,0 +1,23 @@
+#Word list for the hangman game that gets imported into run.py for a random word to be selected
+
+words = [
+    "Zebra",
+    "Skyscraper",
+    "Cellphone",
+    "Apple",
+    "Television",
+    "Bicycle",
+    "Elephant",
+    "Wolf",
+    "Tiger",
+    "Mountain",
+    "Waterfall",
+    "Volcano",
+    "Beaver",
+    "Coffee",
+    "Handbag",
+    "Python",
+    "Blanket",
+    "Keyboard",
+    "Diamond",
+    "Pineapple"]
